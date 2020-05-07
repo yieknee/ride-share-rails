da3e8b359bc574b81af58fd2989779b8a8ae68b2
Yieni <yaynee8d@gmail.com>
2020-05-06 21:34:55 -0700

    set up seed in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_025736) do
+ActiveRecord::Schema.define(version: 2020_05_07_043125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_025736) do
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
-    t.string "phone_number"
+    t.string "phone_num"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

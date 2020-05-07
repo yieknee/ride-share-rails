8ef1b3e3559c131b3f351c8e70ccb6f38cb225a3
ChelseaC13 <chelhelg@outlook.com>
2020-05-07 15:15:27 -0700

    show pages for driver and passenger made

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,14 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id]
+    @driver= Driver.find_by(id: driver_id)
+    if @driver.nil?
+      redirect_to root_path
+      return
+    end
+  end
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,18 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+
+    if @passenger.nil?
+      head :not_found
+      # redirect_to root_path
+      return
+    end
+
+  end
+
 end

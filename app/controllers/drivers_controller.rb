0934a2dfd5d0aeae74ad4416021474e466b20ec7
Yieni <yaynee8d@gmail.com>
2020-05-07 22:26:01 -0700

    got edit, create, and delete buttons working

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -8,8 +8,66 @@ class DriversController < ApplicationController
     @driver= Driver.find_by(id: driver_id)
     @trips = Trip.where(driver_id: driver_id)
     if @driver.nil?
-      redirect_to root_path
+      redirect_to drivers_path
       return
     end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params) 
+    if @driver.save 
+      redirect_to driver_path(@driver.id)
+      return
+    else 
+      render :new
+      return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to drivers_path 
+      return
+    else 
+      render :edit 
+      return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to driver_path
+      return
+    end
+
+    @driver.destroy
+    redirect_to driver_path 
+    return
+    
+  end
+
+ 
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
   end
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -8,11 +8,68 @@ class PassengersController < ApplicationController
     @passenger = Passenger.find_by(id: passenger_id)
     @trips = Trip.where(passenger_id: passenger_id)
     if @passenger.nil?
-      head :not_found
-      # redirect_to root_path
+      redirect_to passengers_path
       return
     end
 
   end
 
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else 
+      render :new
+      return
+    end
+  end
+
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passengers_path # go to the index so we can see the passenger in the list
+      return
+    else # save failed :(
+      render :edit # show the new passenger form view again
+      return
+    end
+  end
+
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      redirect_to passenger_path
+      return
+    end
+
+    @passenger.destroy
+    redirect_to passenger_path 
+    return
+    
+  end
+
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end

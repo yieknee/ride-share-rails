085c46a1c96aebf69bb77c3acca1794f680f71bb
ChelseaC13 <chelhelg@outlook.com>
2020-05-12 11:29:31 -0700

    create test made in trips

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,16 +2,47 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    # # Arrange
+    # passenger = Trips.create(driver_id: '1', passenger_id: '2')
+    # # Act
+    # get(passenger_path(passenger.id))
+    # # Assert
+    # must_respond_with :success
   end
 
-  describe "create" do
-    # Your tests go here
+  describe "create" do 
+
+    it "trip increase by one and redirects" do
+    passenger = Passenger.create!(name: "bob", phone_num:"12334556")
+    driver = Driver.create!(name: "tom", vin:"asdfghjkl", available: true)
+      
+    expect{post passenger_trips_path(passenger.id)}.must_differ "Trip.count", 1
+    must_redirect_to passenger_path(passenger.id)
+    end
   end
 
   describe "edit" do
-    # Your tests go here
-  end
+
+  # let (:new_trip_hash) {
+  #   {
+  #     driver: {
+  #       rating: "3",
+  #     },
+  #   }
+  # }
+  #   it "trip edits rating" do
+  #     trip = Trip.create
+        
+  #     expect {
+  #       patch trip_path(id), params: new_trip_hash
+  #     }.wont_change "Trip.count"
+  
+  #     must_respond_with :redirect
+  
+  #     trip = Trip.find_by(id: id)
+  #     expect(trip.rating).must_equal new_trip_hash[:trip][:rating]
+      
+  # end
 
   describe "update" do
     # Your tests go here
